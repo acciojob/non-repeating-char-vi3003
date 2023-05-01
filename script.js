da5f141d1066d77e1374294731a89d2add0a7f33
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-05-02 01:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let str=prompt("Enter a string");
+
+function firstNonRepeatingCharacter(str) {
+  let charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]]) {
+      charCount[str[i]]++;
+    } else {
+      charCount[str[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+  return null;
+}
+
+let ans=firstNonRepeatingCharacter(str);
+
+alert(ans);
